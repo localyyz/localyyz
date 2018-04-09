8e191a6923465b749676dbe336a3528dd6e1e423
Paul Xue <xuepaul.xp@gmail.com>
2018-04-09 16:02:30 -0400

    ios and android, pods, assets, AppDelegate

--- android/app/src/main/java/com/localyyz/MainApplication.java
+++ android/app/src/main/java/com/localyyz/MainApplication.java
@@ -3,6 +3,25 @@ package com.localyyz;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
+import io.branch.rnbranch.RNBranchPackage;
+import com.reactnativepayments.ReactNativePaymentsPackage;
+import com.reactnative.photoview.PhotoViewPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.cmcewen.blurview.BlurViewPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.cmcewen.blurview.BlurViewPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -14,6 +33,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -22,7 +47,26 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
+            new RNBranchPackage(),
+            new ReactNativePaymentsPackage(),
+            new PhotoViewPackage(),
+            new VectorIconsPackage(),
+            new LinearGradientPackage(),
+            new FBSDKPackage(),
+            new RNDeviceInfo(),
+            new BlurViewPackage(),
+            new VectorIconsPackage(),
+            new LinearGradientPackage(),
+            new RNDeviceInfo(),
+            new BlurViewPackage(),
+            new VectorIconsPackage(),
+            new LinearGradientPackage(),
+            new RNDeviceInfo(),
+            new VectorIconsPackage(),
+            new LinearGradientPackage(),
+            new VectorIconsPackage()
       );
     }
 
--- ios/localyyz/AppDelegate.m
+++ ios/localyyz/AppDelegate.m
@@ -1,10 +1,8 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 #import "AppDelegate.h"
@@ -12,26 +10,126 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+// Branch
+#import <react-native-branch/RNBranch.h>
+
+// Facebook
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBNotifications/FBNotifications.h>
+#import <UserNotifications/UserNotifications.h>
+
+// CodePush
+#import <CodePush/CodePush.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  if (RCT_DEBUG == 1) {
+    //jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios&dev=true"];
+  } else {
+    jsCodeLocation = [CodePush bundleURL];
+  }
 
+  NSString *apiUrl = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"API_URL"];
+  NSDictionary *props = @{@"API_URL" : apiUrl};
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"localyyz"
-                                               initialProperties:nil
+                                               initialProperties:props
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  // replace RN load view with the launch screen
+  UIView* launchScreenView = [[[NSBundle mainBundle] loadNibNamed:@"LaunchScreen" owner:self options:nil] objectAtIndex:0];
+  launchScreenView.frame = self.window.bounds;
+  rootView.loadingView = launchScreenView;
+
+  // facebook
+  [[FBSDKApplicationDelegate sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+
+  // branch
+  [RNBranch initSessionWithLaunchOptions:launchOptions isReferrable:YES];
+
+  // push notification
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  center.delegate = self;
+  [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    if( !error ) {
+      // required to get the app to do anything at all about push notifications
+      [[UIApplication sharedApplication] registerForRemoteNotifications];
+      NSLog( @"Push registration success.");
+    } else {
+      NSLog( @"Push registration FAILED" );
+      NSLog( @"ERROR: %@ - %@", error.localizedFailureReason, error.localizedDescription );
+      NSLog( @"SUGGESTIONS: %@ - %@", error.localizedRecoveryOptions, error.localizedRecoverySuggestion );
+    }
+  }];
+
   return YES;
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler {
+  FBNotificationsManager *notificationsManager = [FBNotificationsManager sharedManager];
+  [notificationsManager presentPushCardForRemoteNotificationPayload:userInfo
+                                                 fromViewController:nil
+                                                         completion:^(FBNCardViewController * _Nullable viewController, NSError * _Nullable error) {
+                                                           if (error) {
+                                                             completionHandler(UIBackgroundFetchResultFailed);
+                                                           } else {
+                                                             completionHandler(UIBackgroundFetchResultNewData);
+                                                           }
+                                                         }];
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  
+  BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                                openURL:url
+                                                      sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                                                             annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+                  ];
+  // Add any custom logic here.
+  return handled;
+}
+
+// track opening and resuming the application attached to the push notification
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+  [FBSDKAppEvents logPushNotificationOpen:userInfo];
+}
+
+// track action performed that was attached to the push notification
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo
+  completionHandler:(void (^)())completionHandler {
+  [FBSDKAppEvents logPushNotificationOpen:userInfo action:identifier];
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *restorableObjects))restorationHandler {
+  return [RNBranch continueUserActivity:userActivity];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  NSString *token = [[deviceToken description] stringByTrimmingCharactersInSet: [NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+  token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
+  NSLog(@"content--- %@", token);
+  [FBSDKAppEvents setPushNotificationsDeviceToken:deviceToken];
+}
+
+// Facebook SDK
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  [FBSDKAppEvents activateApp];
 }
 
 @end
